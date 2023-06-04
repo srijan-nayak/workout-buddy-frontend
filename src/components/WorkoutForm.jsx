5b04c83c5779a918325825afdaa1ed48ee3076a0
Srijan Nayak <srijannayak0@gmail.com>
2023-06-04 11:42:57 +0530

    Add workout form

---
+++ src/components/WorkoutForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+  const [title, setTitle] = useState(null);
+  const [load, setLoad] = useState(null);
+  const [reps, setReps] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const workout = { title, load, reps };
+    const response = await fetch(
+      import.meta.env.VITE_API_URL + "/api/workouts",
+      {
+        method: "POST",
+        body: JSON.stringify(workout),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
+      return;
+    }
+    setTitle("");
+    setLoad("");
+    setReps("");
+    setError("");
+    console.log("new workout added", json);
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a New Workout</h3>
+      <label htmlFor="title">Exercise Title:</label>
+      <input
+        type="text"
+        id="title"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <label htmlFor="load">Load (in kg):</label>
+      <input
+        type="number"
+        id="load"
+        onChange={(e) => setLoad(+e.target.value)}
+        value={load}
+      />
+      <label htmlFor="reps">Reps:</label>
+      <input
+        type="number"
+        id="reps"
+        onChange={(e) => setReps(+e.target.value)}
+        value={reps}
+      />
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
