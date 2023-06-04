4d7be6fe22eab44673c696a42e581fed2b30bb8f
Srijan Nayak <srijannayak0@gmail.com>
2023-06-04 12:29:08 +0530

    Handle deleting workouts

--- src/components/WorkoutDetails.jsx
+++ src/components/WorkoutDetails.jsx
@@ -1,4 +1,21 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch(
+      import.meta.env.VITE_API_URL + "/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +28,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 };
