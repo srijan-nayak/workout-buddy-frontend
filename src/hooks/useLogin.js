60284997571d5cf685bf61edbd9b9ab8285b2795
Srijan Nayak <srijannayak0@gmail.com>
2023-06-24 20:36:14 +0530

    Create useLogin hook

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch(
+      import.meta.env.VITE_API_URL + "/api/users/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+    } else {
+      localStorage.setItem("user", JSON.stringify(json));
+      dispatch({ type: "LOGIN", payload: json });
+    }
+    setIsLoading(false);
+  };
+
+  return { login, isLoading, error };
+};
