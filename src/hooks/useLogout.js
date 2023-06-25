c2b6948c4f78b68d23fdebb5f5ad07592a148329
Srijan Nayak <srijannayak0@gmail.com>
2023-06-25 12:41:01 +0530

    Clear workouts on log out

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutContext";
 
 export const useLogout = () => {
-  const { dispatch } = useAuthContext();
+  const { dispatch: authDispatch } = useAuthContext();
+  const { dispatch: workoutDispatch } = useWorkoutsContext();
 
   const logout = () => {
     localStorage.removeItem("user");
-    dispatch({ type: "LOGOUT" });
+    authDispatch({ type: "LOGOUT" });
+    workoutDispatch({ type: "SET_WORKOUTS", payload: null });
   };
 
   return { logout };
