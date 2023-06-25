b457fbb9257df4f59ae0a8170f3383d4711a82bc
Srijan Nayak <srijannayak0@gmail.com>
2023-06-25 12:00:18 +0530

    Protect routes according to auth state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <BrowserRouter>
       <Navbar />
       <div className="pages">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
